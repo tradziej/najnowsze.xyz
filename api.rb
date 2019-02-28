d83d2d34d49e91c8a86be2d5f339a622b2cbadd8
Tomasz Radziejewski <tomasz@radziejewski.pl>
2019-02-28 15:56:03 +0100

    Correct response headers on API lambda function
    It handles CORS now

--- api.rb
+++ api.rb
@@ -4,6 +4,10 @@ require_relative 'models/item'
 def handler(event:, context:)
   {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.generate({
       items: Item.recent
     })
