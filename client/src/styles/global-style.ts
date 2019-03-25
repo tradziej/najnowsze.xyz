0946b24e28a6aaf2a66a16ce0e8523de0ff852a6
Tomasz Radziejewski <tomasz@radziejewski.pl>
2019-03-25 22:26:02 +0100

    Remove dark-light mode transition animation

--- client/src/styles/global-style.ts
+++ client/src/styles/global-style.ts
@@ -25,7 +25,6 @@ export const GlobalStyle = createGlobalStyle`
 
   body.light-mode {
     color: ${props => props.theme.colors.charlie};
-    transition: background-color 0.3s ease;
   }
 
   body.dark-mode {
