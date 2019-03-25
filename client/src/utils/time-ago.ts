fe4e18955a745c6a249fb5b6660876ba24486af8
Tomasz Radziejewski <tomasz@radziejewski.pl>
2019-03-25 22:18:12 +0100

    Use ceiling function at time ago util

--- client/src/utils/time-ago.ts
+++ client/src/utils/time-ago.ts
@@ -1,9 +1,9 @@
 export default function timeAgo(date: Date): String {
   const today = new Date();
-  const seconds = Math.round((today.valueOf() - date.valueOf()) / 1000);
-  const minutes = Math.round(seconds / 60);
-  const hours = Math.round(minutes / 60);
-  const days = Math.round(hours / 24);
+  const seconds = Math.ceil((today.valueOf() - date.valueOf()) / 1000);
+  const minutes = Math.ceil(seconds / 60);
+  const hours = Math.ceil(minutes / 60);
+  const days = Math.ceil(hours / 24);
 
   if (minutes < 60) {
     return `${minutes}min`;
